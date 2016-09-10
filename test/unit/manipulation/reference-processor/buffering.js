67c0dbbe4d6ce6e5b382f6086cb5baf1f91f3951
Gnucki <tprelot@gmail.com>
2016-09-10 12:00:00 +0200

    Upgrade references resolving

--- test/unit/manipulation/reference-processor/buffering.js
+++ test/unit/manipulation/reference-processor/buffering.js
@@ -6,112 +6,139 @@ require('../../../../lib/common/init');
 require('../path-resolver');
 require('../serializer');
 require('../resolving-stream');
+require('../math-evaluator');
 
 var assert = require('assert'),
     ReferenceProcessor = require('../../../../lib/common/manipulation/reference-processor/buffering'),
     Serializer = require('../../../../lib/common/manipulation/serializer'),
     PathResolver = require('../../../../lib/common/manipulation/path-resolver'),
-    ResolvingStream = require('../../../../lib/common/manipulation/resolving-stream')
+    ResolvingStream = require('../../../../lib/common/manipulation/resolving-stream'),
+    HierarchyEvaluator = require('../../../../lib/common/manipulation/hierarchy-evaluator'),
+    ConditionEvaluator = require('../../../../lib/common/manipulation/condition-evaluator'),
+    MathEvaluator = require('../../../../lib/common/manipulation/math-evaluator')
 ;
 
 var referenceProcessor = new ReferenceProcessor(),
     serializer = new Serializer(),
-    pathResolver = new PathResolver()
+    pathResolver = new PathResolver(),
+    hierarchyEvaluator = new HierarchyEvaluator(),
+    conditionEvaluator = new ConditionEvaluator(),
+    mathEvaluator = new MathEvaluator()
 ;
+
+conditionEvaluator.hierarchyEvaluator = hierarchyEvaluator;
+conditionEvaluator.serializer = serializer;
+
+mathEvaluator.hierarchyEvaluator = hierarchyEvaluator;
 
 referenceProcessor.serializer = serializer;
 referenceProcessor.pathResolver = pathResolver;
+referenceProcessor.conditionEvaluator = conditionEvaluator;
+referenceProcessor.mathEvaluator = mathEvaluator;
 
-var parsingTests = [
+var source = '...',
+    tests = [
         {
             serializedParameters: 'foo0',
-            expected: {
+            parameters: {
                 buffer: 'foo0',
                 source: 'function'
-            }
+            },
+            expected: source
         },
         {
             serializedParameters: 'bar,2',
-            expected: {
+            parameters: {
                 buffer: 'bar',
                 source: 2
-            }
-        },
-        {
-            serializedParameters: 'foo1,.',
-            expected: {
-                buffer: 'foo1',
-                source: 'function'
-            }
-        },
-        {
-            serializedParameters: 'foo2,$',
-            expected: {
-                buffer: 'foo2',
-                source: 'function'
-            }
-        },
-        {
-            serializedParameters: 'foo3,>_',
-            expected: {
-                buffer: 'foo3',
-                source: 'function'
-            }
-        },
-        {
-            serializedParameters: 'foo4,"$"',
-            expected: {
-                buffer: 'foo4',
-                source: 'function'
-            }
-        }
-    ]
-;
-
-var source = '...',
-    processingTests = [
-        {
-            parameters: parsingTests[0].expected,
-            expected: source
-        },
-        {
-            parameters: parsingTests[1].expected,
+            },
             expected: 2
         },
         {
-            parameters: parsingTests[2].expected,
+            serializedParameters: 'foo1,.',
+            parameters: {
+                buffer: 'foo1',
+                source: 'function'
+            },
             expected: source
         },
         {
-            parameters: parsingTests[3].expected,
+            serializedParameters: 'foo2,$',
+            parameters: {
+                buffer: 'foo2',
+                source: 'function'
+            },
             expected: source
         },
         {
-            parameters: parsingTests[4].expected,
+            serializedParameters: 'foo3,>_',
+            parameters: {
+                buffer: 'foo3',
+                source: 'function'
+            },
             expected: source
         },
         {
-            parameters: parsingTests[5].expected,
+            serializedParameters: 'foo4,"$"',
+            parameters: {
+                buffer: 'foo4',
+                source: 'function'
+            },
             expected: '$'
+        },
+        {
+            serializedParameters: 'foo5,2+4',
+            parameters: {
+                buffer: 'foo5',
+                source: 'function'
+            },
+            expected: 6
+        },
+        {
+            serializedParameters: 'foo6,(2(4+1*3))',
+            parameters: {
+                buffer: 'foo6',
+                source: 'function'
+            },
+            expected: 14
+        },
+        {
+            serializedParameters: 'foo7,(2(4+1*3))?1===2',
+            parameters: {
+                buffer: 'foo7',
+                source: 'function',
+                condition: 'function'
+            },
+            expected: source
+        },
+        {
+            serializedParameters: 'foo8,4+1?1<=2',
+            parameters: {
+                buffer: 'foo8',
+                source: 'function',
+                condition: 'function'
+            },
+            expected: 5
         }
     ]
 ;
 
 describe('SelectionReferenceProcessor', function() {
-    parsingTests.forEach(function(test) {
-        it('method "parseParameters" should parse serialized parameters "{0}" in `{1}`'.format(test.serializedParameters, JSON.stringify(test.expected)), function() {
+    tests.forEach(function(test) {
+        it('method "parseParameters" should parse serialized parameters "{0}" in `{1}`'.format(test.serializedParameters, JSON.stringify(test.parameters)), function() {
             var parameters = referenceProcessor.parseParameters(test.serializedParameters);
 
             // Overwrite expected functions.
-            if ('function' === test.expected.source) {
-                test.expected.source = parameters.source;
+            if ('function' === test.parameters.source) {
+                test.parameters.source = parameters.source;
+            }
+            if ('function' === test.parameters.condition) {
+                test.parameters.condition = parameters.condition;
             }
 
-            assert.deepEqual(parameters, test.expected);
+            assert.deepEqual(parameters, test.parameters);
         });
-    });
 
-    processingTests.forEach(function(test) {
         it('method "process" should process `{0}` in `{1}`'.format(JSON.stringify(test.parameters), JSON.stringify(test.expected)), function() {
             var resolvingStream = new ResolvingStream();
 
@@ -121,7 +148,8 @@ describe('SelectionReferenceProcessor', function() {
             assert.deepEqual(resolvingStream.context, test.expected);
 
             resolvingStream.switch();
-            assert.deepEqual(resolvingStream.context, '...');
+
+            assert.deepEqual(resolvingStream.context, source);
         });
     });
 });
--- test/unit/manipulation/reference-processor/selection.js
+++ test/unit/manipulation/reference-processor/selection.js
@@ -6,70 +6,92 @@ require('../../../../lib/common/init');
 require('../path-resolver');
 require('../serializer');
 require('../resolving-stream');
+require('../math-evaluator');
 
 var assert = require('assert'),
     ReferenceProcessor = require('../../../../lib/common/manipulation/reference-processor/selection'),
     Serializer = require('../../../../lib/common/manipulation/serializer'),
     PathResolver = require('../../../../lib/common/manipulation/path-resolver'),
-    ResolvingStream = require('../../../../lib/common/manipulation/resolving-stream')
+    ResolvingStream = require('../../../../lib/common/manipulation/resolving-stream'),
+    HierarchyEvaluator = require('../../../../lib/common/manipulation/hierarchy-evaluator'),
+    ConditionEvaluator = require('../../../../lib/common/manipulation/condition-evaluator'),
+    MathEvaluator = require('../../../../lib/common/manipulation/math-evaluator')
 ;
 
 var referenceProcessor = new ReferenceProcessor(),
     serializer = new Serializer(),
-    pathResolver = new PathResolver()
+    pathResolver = new PathResolver(),
+    hierarchyEvaluator = new HierarchyEvaluator(),
+    conditionEvaluator = new ConditionEvaluator(),
+    mathEvaluator = new MathEvaluator()
 ;
+
+conditionEvaluator.hierarchyEvaluator = hierarchyEvaluator;
+conditionEvaluator.serializer = serializer;
+
+mathEvaluator.hierarchyEvaluator = hierarchyEvaluator;
 
 referenceProcessor.serializer = serializer;
 referenceProcessor.pathResolver = pathResolver;
+referenceProcessor.conditionEvaluator = conditionEvaluator;
+referenceProcessor.mathEvaluator = mathEvaluator;
 
-var parsingTests = [
+var tests = [
         {
             serializedParameters: '',
-            expected: {
+            parameters: {
                 selectors: [
                     {
                         type: 'property',
                         path: ''
                     }
                 ]
-            }
+            },
+            source: {foo: 'bar'},
+            expected: {foo: 'bar'}
         },
         {
             serializedParameters: '.',
-            expected: {
+            parameters: {
                 selectors: [
                     {
                         type: 'property',
                         path: '.'
                     }
                 ]
-            }
+            },
+            source: {foo: 'bar'},
+            expected: ['bar']
         },
         {
             serializedParameters: 'a',
-            expected: {
+            parameters: {
                 selectors: [
                     {
                         type: 'property',
                         path: 'a'
                     }
                 ]
-            }
+            },
+            source: {a: 1},
+            expected: 1
         },
         {
             serializedParameters: 'a.b',
-            expected: {
+            parameters: {
                 selectors: [
                     {
                         type: 'property',
                         path: 'a.b'
                     }
                 ]
-            }
+            },
+            source: {a: {b: '2'}},
+            expected: '2'
         },
         {
             serializedParameters: 'a.b.get()',
-            expected: {
+            parameters: {
                 selectors: [
                     {
                         type: 'property',
@@ -81,11 +103,21 @@ var parsingTests = [
                         args: []
                     }
                 ]
-            }
+            },
+            source: {
+                a: {
+                    b: {
+                        get: function() {
+                            return 'foo';
+                        }
+                    }
+                }
+            },
+            expected: 'foo'
         },
         {
             serializedParameters: 'a.b.convert("42")',
-            expected: {
+            parameters: {
                 selectors: [
                     {
                         type: 'property',
@@ -97,11 +129,21 @@ var parsingTests = [
                         args: ["42"]
                     }
                 ]
-            }
+            },
+            source: {
+                a: {
+                    b: {
+                        convert: function(value) {
+                            return +value;
+                        }
+                    }
+                }
+            },
+            expected: 42
         },
         {
             serializedParameters: 'a.b.concat("foo", 2, $c, .c)',
-            expected: {
+            parameters: {
                 selectors: [
                     {
                         type: 'property',
@@ -113,11 +155,23 @@ var parsingTests = [
                         args: ["foo", 2, 'function', 'function']
                     }
                 ]
-            }
+            },
+            source: {
+                a: {
+                    b: {
+                        concat: function() {
+                            return Array.prototype.slice.call(arguments).join('-');
+                        },
+                        c: 'bar'
+                    }
+                },
+                c: 'foobar'
+            },
+            expected: 'foo-2-foobar-bar'
         },
         {
             serializedParameters: 'a.b.get().c.d',
-            expected: {
+            parameters: {
                 selectors: [
                     {
                         type: 'property',
@@ -133,74 +187,7 @@ var parsingTests = [
                         path: 'c.d'
                     }
                 ]
-            }
-        }
-    ]
-;
-
-var processingTests = [
-        {
-            source: {foo: 'bar'},
-            parameters: parsingTests[0].expected,
-            expected: {foo: 'bar'}
-        },
-        {
-            source: {foo: 'bar'},
-            parameters: parsingTests[1].expected,
-            expected: ['bar']
-        },
-        {
-            source: {a: 1},
-            parameters: parsingTests[2].expected,
-            expected: 1
-        },
-        {
-            source: {a: {b: '2'}},
-            parameters: parsingTests[3].expected,
-            expected: '2'
-        },
-        {
-            source: {
-                a: {
-                    b: {
-                        get: function() {
-                            return 'foo';
-                        }
-                    }
-                }
             },
-            parameters: parsingTests[4].expected,
-            expected: 'foo'
-        },
-        {
-            source: {
-                a: {
-                    b: {
-                        convert: function(value) {
-                            return +value;
-                        }
-                    }
-                }
-            },
-            parameters: parsingTests[5].expected,
-            expected: 42
-        },
-        {
-            source: {
-                a: {
-                    b: {
-                        concat: function() {
-                            return Array.prototype.slice.call(arguments).join('-');
-                        },
-                        c: 'bar'
-                    }
-                },
-                c: 'foobar'
-            },
-            parameters: parsingTests[6].expected,
-            expected: 'foo-2-foobar-bar'
-        },
-        {
             source: {
                 a: {
                     b: {
@@ -216,20 +203,149 @@ var processingTests = [
                 },
                 c: 'foobar'
             },
-            parameters: parsingTests[7].expected,
             expected: 4
+        },
+        {
+            serializedParameters: 'a.b+2*.a.c',
+            parameters: {
+                selectors: [
+                    {
+                        type: 'property',
+                        path: 'a.b'
+                    }
+                ],
+                operation: '+2*.a.c'
+            },
+            source: {
+                a: {
+                    b: 3,
+                    c: 4
+                }
+            },
+            expected: 11
+        },
+        {
+            serializedParameters: 'a.b.get().c.d+2(.e*2)',
+            parameters: {
+                selectors: [
+                    {
+                        type: 'property',
+                        path: 'a.b'
+                    },
+                    {
+                        type: 'method',
+                        method: 'get',
+                        args: []
+                    },
+                    {
+                        type: 'property',
+                        path: 'c.d'
+                    }
+                ],
+                operation: '+2(.e*2)'
+            },
+            source: {
+                a: {
+                    b: {
+                        get: function() {
+                            return {
+                                c: {
+                                    d: 4,
+                                    e: 5
+                                }
+                            };
+                        }
+                    }
+                },
+                e: 8
+            },
+            expected: 36
+        },
+        {
+            serializedParameters: 'foo?.bar==="bar"',
+            parameters: {
+                selectors: [
+                    {
+                        type: 'property',
+                        path: 'foo'
+                    },
+                ],
+                condition: 'function'
+            },
+            source: {
+                foo: 'bar',
+                bar: 'foo'
+            },
+            expected: {
+                foo: 'bar',
+                bar: 'foo'
+            }
+        },
+        {
+            serializedParameters: 'foo?.bar==="foo"',
+            parameters: {
+                selectors: [
+                    {
+                        type: 'property',
+                        path: 'foo'
+                    },
+                ],
+                condition: 'function'
+            },
+            source: {
+                foo: 'bar',
+                bar: 'foo'
+            },
+            expected: 'bar'
+        },
+        {
+            serializedParameters: 'bar?.bar==="foo"&&.foo==="foo"',
+            parameters: {
+                selectors: [
+                    {
+                        type: 'property',
+                        path: 'bar'
+                    },
+                ],
+                condition: 'function'
+            },
+            source: {
+                foo: 'bar',
+                bar: 'foo'
+            },
+            expected: {
+                foo: 'bar',
+                bar: 'foo'
+            }
+        },
+        {
+            serializedParameters: 'bar?.bar==="foo"||.foo==="foo"',
+            parameters: {
+                selectors: [
+                    {
+                        type: 'property',
+                        path: 'bar'
+                    },
+                ],
+                condition: 'function'
+            },
+            source: {
+                foo: 'bar',
+                bar: 'foo'
+            },
+            expected: 'foo'
         }
     ]
 ;
 
 describe('SelectionReferenceProcessor', function() {
-    parsingTests.forEach(function(test) {
-        it('method "parseParameters" should parse serialized parameters "{0}" in `{1}`'.format(test.serializedParameters, JSON.stringify(test.expected)), function() {
+    tests.forEach(function(test) {
+        it('method "parseParameters" should parse serialized parameters "{0}" in `{1}`'.format(test.serializedParameters, JSON.stringify(test.parameters)), function() {
             var parameters = referenceProcessor.parseParameters(test.serializedParameters);
 
             // Overwrite expected functions.
-            for (var i = 0; i < test.expected.selectors.length; i++) {
-                var selector = test.expected.selectors[i];
+            for (var i = 0; i < test.parameters.selectors.length; i++) {
+                var selector = test.parameters.selectors[i];
 
                 if (selector.args) {
                     selector.args = selector.args.map(function(arg, index) {
@@ -240,12 +356,13 @@ describe('SelectionReferenceProcessor', function() {
                     });
                 }
             }
+            if ('function' === test.parameters.condition) {
+                test.parameters.condition = parameters.condition;
+            }
 
-            assert.deepEqual(parameters, test.expected);
+            assert.deepEqual(parameters, test.parameters);
         });
-    });
 
-    processingTests.forEach(function(test) {
         it('method "process" should process `{0}` in `{1}`'.format(JSON.stringify(test.source), JSON.stringify(test.expected)), function() {
             var resolvingStream = new ResolvingStream();
 
@@ -254,5 +371,8 @@ describe('SelectionReferenceProcessor', function() {
 
             assert.deepEqual(resolvingStream.context, test.expected);
         });
+    });
+
+    tests.forEach(function(test) {
     });
 });
